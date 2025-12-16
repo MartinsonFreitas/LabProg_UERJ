1252d953dcfeffe0989f2eb538df4fc7eff38257
Martinson Freitas <martinson.freitas@gmail.com>
2025-12-16 20:21:13 -0300

    Add files via upload

---
+++ Exercicios/Ponteiros/ponteiro1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define N sizeof(meu_array) / sizeof(meu_array[0])
+
+int meu_array[] = {1, 23, 17, 4, -5, 100};
+int *ptr;
+
+int main(void) {
+	ptr = &meu_array[0]; /* aponta nosso ponteiro para o primeiro
+							elemento do array */
+							
+	for (int i = 0; i < N; i++) {
+		printf("meu_array[%d] = %d\t", i, meu_array[i]); 	/*<-- A */
+		//printf("ptr + %d = %d\n", i, *(ptr + i));			/*<-- B */
+		//printf("ptr + %d = %d\n",i, *ptr++);
+		printf("ptr + %d = %d\n",i, *(++ptr));
+	}
+
+	return 0;
+}
